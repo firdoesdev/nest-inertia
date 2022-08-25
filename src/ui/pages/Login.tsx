02b297a6c891d7ca77e89f26d240e81f089eac29
Adawiyyah Latifah <adawiyyahlatifah@gmail.com>
2022-08-25 21:04:39 +0700

    fix: submit handler

--- src/ui/pages/Login.tsx
+++ src/ui/pages/Login.tsx
@@ -3,7 +3,7 @@ import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import { Link, useForm } from '@inertiajs/inertia-react';
 import { Button, Checkbox, Form, Input, Layout } from 'antd';
 import { Content } from 'antd/lib/layout/layout';
-import {FC} from 'react';
+import React, {FC} from 'react';
 
 const Login: FC = () => {
     const {
@@ -15,12 +15,18 @@ const Login: FC = () => {
     } = useForm({
         email: '',
         password: '',
-        remember: false,
     })
 
-    const handleSubmit = (event) => {
-        event.prefentDefault()
-        Inertia.post('/verified')
+    const handleChanges = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+        setData({...data, [name]: value})
+    }
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        // event.preventDefault()
+        console.log(data.email)
+        console.log(data.password)
+        post('/verified')
     }
 
     return (
@@ -36,31 +42,27 @@ const Login: FC = () => {
                 <Form
                     name="login"
                     onFinish={handleSubmit}
-                    style={{maxWidth: "300px"}}
+                    style={{minWidth: "300px"}}
                 >
                     <Form.Item
                         name="email"
                         rules={[{required: true, message: 'Please input your email!'}]}
                     >
-                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" id='email' value={data.email} type="text" onChange={event => setData('email', event.target.value)}/>
-                        {errors.email && <div>{errors.email}</div>}
+                        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Email" id='email' name='email' type="text" value={data.email} onChange={handleChanges}/>
+                        {/* {errors.email && <div>{errors.email}</div>} */}
                     </Form.Item>
 
                     <Form.Item
                         name="password"
                         rules={[{ required: true, message: 'Please input your Password!' }]}
                     >
-                        <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" id='password' value={data.password} onChange={event => setData('password', event.target.value)}/>
-                        {errors.password && <div>{errors.password}</div>}
+                        <Input prefix={<LockOutlined className="site-form-item-icon" />} type="password" placeholder="Password" id='password' name='password' value={data.password} onChange={handleChanges}/>
+                        {/* {errors.password && <div>{errors.password}</div>} */}
                     </Form.Item>
 
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox checked={data.remember} onChange={event => setData('remember', event.target.checked)}>Remember me</Checkbox>
-                    </Form.Item>
-                    
                     <Form.Item>
                         <Button type="primary" htmlType="submit" disabled={processing} style={{width: "100%"}}>Login</Button>
-                        Or <Link href={'/register'}>Register now!</Link>
+                        <p style={{textAlign: 'center'}}>or <Link href={'/register'}>Register now!</Link></p> 
                     </Form.Item>
                 </Form>
             </Content>
